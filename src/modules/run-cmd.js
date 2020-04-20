54b0325b1ebed3bd9265e2d90f42117e0507a86b
Ernesto Stifano <ernesto.stifano@codistica.com>
2020-04-20 12:56:41 +0200

    [NEW] - Add option to ignore stdio in run-cmd module.

--- src/modules/run-cmd.js
+++ src/modules/run-cmd.js
@@ -1,25 +1,40 @@
 import {spawn} from 'child_process';
 
 /**
+ * @typedef runCmdOptionsType
+ * @property {boolean} [stdioIgnore=false] - Ignore stdin, stdout and stderr of spawned process.
+ */
+
+/**
  * @async
  * @description Programmatically run commands from current working directory.
  * @param {string} cmd - Command string to be ran.
+ * @param {runCmdOptionsType} [options] - Options.
  * @returns {Promise<void>} Promise. Void.
  */
-async function runCmd(cmd) {
+async function runCmd(cmd, options) {
     const cmdArr = cmd.split(' ');
     const cmdObj = {
         cmd: cmdArr[0],
         args: cmdArr.slice(1)
     };
+
+    if (!options) {
+        options = {
+            stdioIgnore: false
+        }
+    } else {
+        options.stdioIgnore = typeof options.stdioIgnore === 'boolean' ? options.stdioIgnore : false;
+    }
+
     await new Promise((resolve, reject) => {
         spawn(cmdObj.cmd, cmdObj.args, {
             cwd: process.cwd(),
-            stdio: ['inherit', 'inherit', 'inherit'],
+            stdio: options.stdioIgnore ? ['ignore', 'ignore', 'ignore'] : ['inherit', 'inherit', 'inherit'],
             shell: process.platform === 'win32'
         }).on('exit', (exitCode) => {
             if (exitCode !== 0) {
-                reject(new Error('COMMAND EXECUTION FAILED'));
+                reject(new Error('PROCESS ENDED WITH A NON 0 EXIT CODE.'));
             }
             resolve();
         });
